3ae476defa5de42dfaf90554b04d3f607a5c63ff
ryoga <surpace2479@gmail.com>
2024-08-07 13:17:29 +0900

    Eigenが実行できることを確認

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <Eigen/Dense>
+
+int main()
+{
+  Eigen::Matrix2d mat;
+  mat << 1, 2, 3, 4;
+  std::cout << "Here is the matrix mat:\n" << mat << std::endl;
+
+  Eigen::Vector2d vec;
+  vec << 5, 6;
+  std::cout << "Here is the vector vec:\n" << vec << std::endl;
+
+  Eigen::Vector2d result = mat * vec;
+  std::cout << "The result of mat * vec is:\n" << result << std::endl;
+
+  return 0;
+}
